2f59c34fb5bc4e0619b4624eb0064a5162656c66
e.a.fomin <evgeniy.fomin@regium.com>
2021-10-23 10:36:35 +0300

    HomeWork 4.3

--- terminal/languages/json_yaml/HomeWork4.3.2.py
+++ terminal/languages/json_yaml/HomeWork4.3.2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import socket
-# import json
-# import yaml
+import json
+import yaml
 
 dnsdrivegoogle = 'drive.google.com'
 dnsmailgoogle = 'mail.google.com'
@@ -11,8 +11,11 @@ ipmailgoogle = socket.gethostbyname(dnsmailgoogle)
 ipgoogle = socket.gethostbyname(dnsgoogle)
 
 ipdict = { dnsdrivegoogle: ipdrivegoogle, dnsmailgoogle: ipmailgoogle, dnsgoogle: ipgoogle}
-# with open("server.json", "w") as js:
-#     js.dict = ipdict
+with open('server.json', 'w') as js:
+    js.write(json.dumps(ipdict, indent=2))
+
+with open('server.yml', 'w') as ym:
+    ym.write(yaml.dump(ipdict, indent=2))
 a=0
 
 while a < 10:
@@ -21,4 +24,10 @@ while a < 10:
             print(f'{i} = {ipdict[i]}')
         else:
             print(f'[ERROR] {i} = {ipdict[i]}')
+            ipdict[i] = socket.gethostbyname(i)
+            with open('server.json', 'w') as js:
+                js.write(json.dumps(ipdict, indent=2))
+
+            with open('server.yml', 'w') as ym:
+                ym.write(yaml.dump(ipdict, indent=2))
     a += 1
