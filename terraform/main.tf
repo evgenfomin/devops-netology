f846c80218307d064d129daeef8cc197b3cb7715
Evgeniy Fomin <evgeniyfomin@MacBook-Pro-Evgeniy.local>
2022-02-07 17:33:16 +0300

    Home Work 7.3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
+  access_key = "AKIASOZKKUHJQ35U6TNZ"
+  secret_key = "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"
   region = "us-east-1"
+}
+
+output "aws_account_id" {
+  value = "169203114451"
+}
+output "aws_user_id" {
+  value = "arn:aws:iam::169203114451:user/Administrator"
 }
 
 data "aws_ami" "ubuntu" {
@@ -35,5 +53,25 @@ resource "aws_instance" "web" {
   
   tags = {
     Name = "my_first_instance"
+  }
+}
+
+locals {
+  instances = {
+    "t3.micro" = data.aws_ami.ubuntu
+    "t3.large" = data.aws_ami.ubuntu
+  }
+}
+resource "aws_instance" "web1" {
+  for_each = local.instances
+  ami = "each.value"
+  instance_type = each.key
+  cpu_core_count = 2
+
+  tags = {
+    Name = "my_first_instance"
+  }
+  lifecycle {
+    create_before_destroy = true
   }
 }
--- terraform/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "aws_account_id" {
-  value = "169203114451"
-}
-output "aws_user_id" {
-  value = "arn:aws:iam::169203114451:user/Administrator"
-}
